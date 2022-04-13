4500f697d93d5c84ca307adcbfdaeb1257a0938a
Dmitrij Lukashenko <dzmitry.lukashenka@itechart-group.com>
2022-04-13 17:23:44 +0300

    working with stop video by close modal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
 const modalWindow = document.querySelector('.modal');
 const buttonsModal = document.querySelectorAll('.button-play')
+const iFrame = document.getElementById('video')
 buttonsModal.forEach((item, i) => {
     item.addEventListener('click', () => {
         modalWindow.classList.add('active')
     })
 })
 
+
 modalWindow.addEventListener('click', () => {
+    console.log(' in app')
     modalWindow.classList.remove('active')
+
 })
