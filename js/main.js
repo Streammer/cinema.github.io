908c1c0406627d8192f98801f3191750ac8d877a
Dmitrij Lukashenko <dzmitry.lukashenka@itechart-group.com>
2022-04-14 10:47:20 +0300

    Music is stopped when the modal window is closed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+window.onload = () => {
+    //add script tag with youtube api to the html page
 
+    let tag = document.createElement('script');
+    tag.src = "//www.youtube.com/player_api";
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+// modal window with iframe
 
 const modalWindow = document.querySelector('.modal');
 const buttonsModal = document.querySelectorAll('.button-play')
-const iFrame = document.getElementById('video')
+const iFrame = document.getElementById('video') //iframe
+
+//set eventListener to the all view buttons through the loop. It is only for demonstration
+
 buttonsModal.forEach((item, i) => {
     item.addEventListener('click', () => {
         modalWindow.classList.add('active')
     })
 })
+//create the Player object
 
+let player;
+
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    player = new YT.Player('video', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+//create the “player ready” callback 
+
+function onPlayerReady() {
+    player.pauseVideo();
+}
+
+//close modal window
 
 modalWindow.addEventListener('click', () => {
-    console.log(' in app')
+    onPlayerReady()
     modalWindow.classList.remove('active')
-
 })
